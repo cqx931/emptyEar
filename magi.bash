4822222806f491abf35ed0ba57b2ccdf8697ab61
cqx931 <cqx931@gmail.com>
2018-05-09 08:51:41 -0400

    fix

--- magi.bash
+++ magi.bash
@@ -1,3 +1,4 @@
+move_position="tell application \"Terminal\" to set bounds of window 1 to {0, 0, 505, 320}"
 run_englishEar="cd emptyear && python ear_demo.py 1"
 run_danishEar="cd emptyear && python ear_demo.py 2"
 run_foreignEar="cd emptyear && python ear_demo.py 3"
