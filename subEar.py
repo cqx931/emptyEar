6ce42f46e821fcf42483c9b1700f08e51fa8e37c
Helsingør Bibliotek <helsbib@Helsingrs-iMac.local>
2018-05-17 14:58:14 +0200

    commit subEar

--- subEar.py
+++ subEar.py
@@ -1,6 +1,6 @@
 import httplib, subprocess
 
-c = httplib.HTTPConnection('localhost', 8080)
-c.request('POST', '/return', '{}')
+c = httplib.HTTPConnection('192.168.204.150', 80)
+c.request('POST', '/hello', '{}')
 doc = c.getresponse().read()
 print doc
