2f7940bbc57701f7fac27c9afcf2ce643e82896f
cqx931 <cqx931@gmail.com>
2018-05-17 17:39:36 +0200

    try

--- server/server.py
+++ server/server.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 # Put this in /Library/WebServer/Documents/
-# sudo python server.py
+from bottle import run
 import bottle
 
 app = application = bottle.default_app()
 
-if __name__ == '__main__':
-    bottle.run(host = '192.168.204.150', port = 80)
+run(app, host='192.168.204.150', port=80)
