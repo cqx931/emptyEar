d1b9d44bc1efcbf1771319679769146ad1637216
helsbib <admin@helsbibs-iMac.local>
2018-05-17 18:12:22 +0200

    change api location

--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@
 
 # Put this in /Library/WebServer/Documents/
 from bottle import Bottle, run
+from bottle import request, response
 
 app = Bottle()
 
@@ -11,4 +12,11 @@ def hello():
 @app.route('/')
 def connect():
     return "Connected!"
+@app.get('/API')
+def listing_handler():
+    #response.headers['Content-Type'] = 'application/json'
+    #esponse.headers['Cache-Control'] = 'no-cache'
+    return "test"
+
+#TODO: API should goes here
 run(app, host='192.168.204.150', port=80)
