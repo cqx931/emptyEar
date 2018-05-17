d94d929dd730b28107f303876ce97ca9f6fc9c33
helsbib <admin@helsbibs-iMac.local>
2018-05-17 17:49:04 +0200

    fix server

--- server/server.py
+++ server/server.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
 # Put this in /Library/WebServer/Documents/
-from bottle import run
-import bottle
+from bottle import Bottle, run
 
-app = application = bottle.default_app()
+app = Bottle()
 
+@app.route('/hello')
+def hello():
+    return "Welcome to the Empty Ear Machine!"
+@app.route('/')
+def connect():
+    return "Connected!"
 run(app, host='192.168.204.150', port=80)
