4ad0ff413af2d4ed357a606c039c98cf9b963174
Helsingør Bibliotek <helsbib@Helsingrs-iMac-2.local>
2018-05-19 09:42:50 +0200

    update bash

--- engMachine.bash
+++
@@ -1,7 +0,0 @@
-run_engEar1="cd emptyear && python3 subEar.py english1"
-run_engEar2="cd emptyear && python3 subEar.py english2"
-run_engEar3="cd emptyear && python3 subEar.py english3"
-
-osascript -e "tell application \"Terminal\" to do script \"$run_engEar1\""
-osascript -e "tell application \"Terminal\" to do script \"$run_engEar2\""
-osascript -e "tell application \"Terminal\" to do script \"$run_engEar3\""
--- intMachine.bash
+++
@@ -1,7 +0,0 @@
-run_foreignEar1="cd emptyear && python3 subEar.py international1"
-run_foreignEar2="cd emptyear && python3 subEar.py international2"
-run_foreignEar3="cd emptyear && python3 subEar.py international3"
-
-osascript -e "tell application \"Terminal\" to do script \"$run_foreignEar1\""
-osascript -e "tell application \"Terminal\" to do script \"$run_foreignEar2\""
-osascript -e "tell application \"Terminal\" to do script \"$run_foreignEar3\""
