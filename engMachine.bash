93f51bd1caaf8b87d21214e1322716cbdefecd98
cqx931 <cqx931@gmail.com>
2018-05-19 03:38:19 +0200

    better bash

--- engMachine.bash
+++ engMachine.bash
@@ -1,6 +1,6 @@
-run_engEar1="cd emptyear && python subEar.py english1"
-run_engEar2="cd emptyear && python subEar.py english2"
-run_engEar3="cd emptyear && python subEar.py english3"
+run_engEar1="cd emptyear && python3 subEar.py english1"
+run_engEar2="cd emptyear && python3 subEar.py english2"
+run_engEar3="cd emptyear && python3 subEar.py english3"
 
 osascript -e "tell application \"Terminal\" to do script \"$run_engEar1\""
 osascript -e "tell application \"Terminal\" to do script \"$run_engEar2\""
--- intMachine.bash
+++ intMachine.bash
@@ -1,6 +1,6 @@
-run_foreignEar1="cd emptyear && python subEar.py international1"
-run_foreignEar2="cd emptyear && python subEar.py international2"
-run_foreignEar3="cd emptyear && python subEar.py international3"
+run_foreignEar1="cd emptyear && python3 subEar.py international1"
+run_foreignEar2="cd emptyear && python3 subEar.py international2"
+run_foreignEar3="cd emptyear && python3 subEar.py international3"
 
 osascript -e "tell application \"Terminal\" to do script \"$run_foreignEar1\""
 osascript -e "tell application \"Terminal\" to do script \"$run_foreignEar2\""
